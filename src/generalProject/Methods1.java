f862b96cd873f89bf25bfe97f926f3edb1fd6eef
AikoSharsheeva <calipsolee@gmail.com>
2023-08-29 19:54:43 +0600

    Methods 1 task

---
+++ src/generalProject/Methods1.java
@@ -0,0 +1,12 @@
+package generalProject;
+
+public class Methods1 {
+    public static void main(String[] args) {
+        Methods1 obj = new Methods1();
+        obj.success();
+    }
+
+    public void success() {
+        System.out.println("I'm taking my success in my own hands!");
+    }
+}
