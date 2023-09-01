540c4a0119d2df5e1b574c6a7d6a179152268bf4
AikoSharsheeva <calipsolee@gmail.com>
2023-09-01 18:38:21 +0600

    While Loop 11 print num

---
+++ src/generalProject/WhileLoop11.java
@@ -0,0 +1,13 @@
+package generalProject;
+
+public class WhileLoop11 {
+    public static void main(String[] args) {
+        int num=1;
+        while(num<=5) {
+
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
