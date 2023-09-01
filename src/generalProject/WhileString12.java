ee8f1bd08b0d504b27b8cc329c12b4f56cff1bf9
AikoSharsheeva <calipsolee@gmail.com>
2023-09-01 18:45:28 +0600

    while with String and num

---
+++ src/generalProject/WhileString12.java
@@ -0,0 +1,15 @@
+package generalProject;
+
+public class WhileString12 {
+    public static void main(String[] args) {
+        String str = "$150k/year Job Offer ";
+        int month = 1;
+        while (month <= 5) {
+            System.out.println(str + month);
+            month++;
+        }
+        System.out.println("Offer from Airbnb Received!");
+    }
+
+}
+
