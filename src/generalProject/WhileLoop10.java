7ae12c665574e72cdaaefef61cb04281485ce029
AikoSharsheeva <calipsolee@gmail.com>
2023-09-01 17:31:58 +0600

    While loop 10 task

---
+++ src/generalProject/WhileLoop10.java
@@ -0,0 +1,12 @@
+package generalProject;
+
+public class WhileLoop10 {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=5){
+            i++;
+            System.out.println("Enjoy to coding");
+        }
+
+    }
+}
