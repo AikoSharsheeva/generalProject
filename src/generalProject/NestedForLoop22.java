b366da8e8645761b98361bb0883f1908160c14ce
AikoSharsheeva <calipsolee@gmail.com>
2023-09-02 18:26:46 +0600

    Nested for loop 22

---
+++ src/generalProject/NestedForLoop22.java
@@ -0,0 +1,12 @@
+package generalProject;
+
+public class NestedForLoop22 {
+    public static void main(String[] args) {
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                System.out.println("*");//выполнится 9 раз каждая звездочка на новой строке
+            }
+        }
+
+    }
+}
