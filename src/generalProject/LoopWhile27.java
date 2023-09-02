59d66b33082f64deff342f1332a2365bc21986c2
AikoSharsheeva <calipsolee@gmail.com>
2023-09-02 21:56:18 +0600

    While Nested Pattern using digits

---
+++ src/generalProject/LoopWhile27.java
@@ -0,0 +1,17 @@
+package generalProject;
+
+public class LoopWhile27 {
+    public static void main(String[] args) {
+        int num=5;
+        while(num>=1){
+
+            int j=num;
+            while(j>=1){
+                System.out.print(j + " ");
+                j--;
+            }
+            num--;
+            System.out.println();
+        }
+    }
+}
