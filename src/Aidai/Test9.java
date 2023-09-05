0783c6c42c448fc162683d942cf11bc1be795792
Aidai <AidaiJk777@gmail.com>
2023-09-05 19:19:53 +0600

    if nested test 9

--- src/Aidai/Test9.java
+++ src/Aidai/Test9.java
@@ -1,7 +1,29 @@
 package Aidai;
 
+import java.util.Scanner;
+
 public class Test9 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Как ваше здоровье? good or bad");
+        String health = sc.nextLine();
+        if (health.equals("good")) {
+            System.out.println("введите вашу физ подготовку high medium low");
+           String fitnessStatus = sc.nextLine();
+           if (fitnessStatus.equals("high")) {
+               System.out.println("ты можешь пробежать целый марафон");
+           } else if (fitnessStatus.equals("medium")) {
+               System.out.println("ты можешь пробежать половину марафона");
+           } else if (fitnessStatus.equals("low")) {
+               System.out.println("ты можешь пробежать 5 км");
+           } else {
+               System.out.println("неправильный ввод");
+           }
+        }
+         else if (health.equals("bad")) {
+            System.out.println("вы можете пройтись");
+        } else {
+            System.out.println("введите ваш статус");
+        }
     }
 }
